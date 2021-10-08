37e547bfd64e01935987821736b51610e86a6a30
lusnig <luca.lusnig@mpi-dortmund.mpg.de>
2021-10-08 13:40:14 +0200

    added function for creating PV simulated values

--- pv_simulator/PV_simulator.py
+++ pv_simulator/PV_simulator.py
@@ -1,2 +1,23 @@
-def dummy_broker():
-    return True
+import numpy as np
+
+def get_sec(t)->int:
+    """
+    transform the given datatime in seconds elapsed in the day
+    :param t: time in datatime (%m-%d-%Y %H:%M:%S)'s format
+    :return: seconds elapsed in the day
+    """
+    h_min_sec=t.split(" ")[1].split(":")
+    return int(h_min_sec[0])*3600 + int(h_min_sec[1])*60 + int(h_min_sec[2])
+
+
+def gauss(x:float, A:float, mu:float, sigma:float)->float:
+    """
+    Return the value of a gaussian function in the point x
+    :param x: point where the value will be calculated
+    :param A: height of the curve's peak
+    :param mu: position of the center of the peak
+    :param sigma: standard deviation
+    :return:
+    """
+    return A * np.exp(-(x - mu) ** 2 / (2 * sigma ** 2))
+
--- tests/test_PV_simulator.py
+++ tests/test_PV_simulator.py
@@ -1,13 +1,16 @@
 import unittest
-
 from pv_simulator import PV_simulator
+from datetime import datetime
 
 
+class gauss_test(unittest.TestCase):
+    def test_gauss(self):
+        self.assertAlmostEqual(1.5966859,PV_simulator.gauss(x=10,A=3.4,mu=13,sigma=2.44),5)
 
-class dummy_test(unittest.TestCase):
-    def test_dummy(self):
-        self.assertTrue(PV_simulator.dummy_broker())
-
+class get_sec_test(unittest.TestCase):
+    def test_get_sec(self):
+        self.assertEqual(53759,PV_simulator.get_sec(datetime(year=2020, month=11, day=28, hour=14, minute=55, second=59).strftime("%m-%d-%Y %H:%M:%S")))
+        self.assertEqual(0,PV_simulator.get_sec(datetime(year=2020, month=11, day=28, hour=0, minute=0, second=0).strftime("%m-%d-%Y %H:%M:%S")))
 
 
 if __name__ == '__main__':
