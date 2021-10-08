711fa80677b191ddd78c833c0fa0d34273365ac7
lusnig <luca.lusnig@mpi-dortmund.mpg.de>
2021-10-08 14:01:46 +0200

    added basic structure of Meter class

--- pv_simulator/Meter.py
+++ pv_simulator/Meter.py
@@ -1,2 +1,39 @@
-def dummy_meter():
-    return True
+"""
+The Meter produces messages to the broker with random but continuous values from 0 to 9000 Watts.
+This is to mock a regular home power consumption
+"""
+
+from pv_simulator.Broker import Broker
+from numpy import random
+
+class Meter:
+    def __init__(self, min_power:float, max_power:float, delta_time: int, broker:Broker)->None:
+        """
+        :param min_power: minimum power value
+        :param max_power: maximum power value
+        :param delta_time: time between the creation of two consecutive messages
+        :param broker: Broker obj
+        """
+        self.min_power = min_power
+        self.max_power = max_power
+        self.delta_time = delta_time
+        self.broker = broker
+
+    def open_connection(self)->None:
+        """
+        Enable the connection to the Broker
+        """
+        pass
+
+    def close_connection(self)->None:
+        """
+        close the connection to the Broker
+        """
+        pass
+
+    def send_messages(self)->None:
+        """
+        Continuously sends messages to the Broker
+        """
+        while True:
+            msg = random.uniform(self.min_power, self.max_power)
